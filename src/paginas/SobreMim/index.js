d50b54be7bcbc6057da9e4933588b5156b34b059
Lucas <lucasv.cornelius@gmail.com>
2024-06-27 15:37:40 -0300

    creating about me

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,47 @@
+import styles from './SobreMim.module.css'
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png"
+import fotoSobreMim from "assets/sobre_mim.jpeg"
+
 export default function SobreMim () {
     return (
-        <h1>Sobre mim...</h1>
+        <PostModelo 
+            fotoCapa={fotoCapa}
+            titulo={"Sobre mim"}
+        >
+
+        <h3 className={styles.subtitulo}>
+            Olá, eu sou o Lucas!
+        </h3>
+
+        <img
+            src={fotoSobreMim}
+            alt='Foto de Lucas Cornelius'
+            className={styles.fotoSobreMim}
+        />
+            <p className={styles.paragrafo}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+
+            <p className={styles.paragrafo}>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p className={styles.paragrafo}>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+
+            <p className={styles.paragrafo}>
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            </p>
+
+            <p className={styles.paragrafo}>
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+            </p>
+
+            <p className={styles.paragrafo}>
+            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+            </p>
+        </PostModelo>
     )
 }
